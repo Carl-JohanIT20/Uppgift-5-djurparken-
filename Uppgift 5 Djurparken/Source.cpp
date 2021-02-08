40656fa9f5e28215232c42f406ebde18a183ff85
Carl-Johan Erlandsson <carljohan.erlandsso@learnet.se>
2021-02-08 15:34:42 +0100

    Add project files.

---
+++ Uppgift 5 Djurparken/Source.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+
+class Zebra
+{
+	float vikt;
+	int age;
+	std::string Diet;
+	int serialnumber;
+	float speed;
+	std::string name;
+};
+
+class Giraff
+{
+	float vikt;
+	int age;
+	std::string Diet;
+	int serialnumber;
+	float speed;
+	std::string name;
+};
+
+class Pingvin
+{
+	float vikt;
+	int age;
+	std::string Diet;
+	int serialnumber;
+	float speed;
+	std::string name;
+};
+
+class Lejon
+{
+	float vikt;
+	int age;
+	std::string Diet;
+	int serialnumber;
+	float speed;
+	std::string name;
+};
+
+class Elephant
+{
+	float vikt;
+	int age;
+	std::string Diet;
+	int serialnumber;
+	float speed;
+	std::string name;
+};
+int main()
+{
+
+
+
+
+
+	return 0;
+}
